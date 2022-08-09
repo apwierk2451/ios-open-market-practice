2e7ccbe4ebce6e2f897b8265e9d21e24aa6ccc4d
ic_6_user_28 <toskw@naver.com>
2022-08-09 16:29:08 +0900

    feat: JSONModel 구현

---
+++ OpenMarket/OpenMarket/Model/JSONModel/Currency.swift
@@ -0,0 +1,11 @@
+//
+//  Currency.swift
+//  OpenMarket
+//
+//  Created by bonf, kiwi on 2022/08/09.
+//
+
+enum Currency: String, Codable {
+    case krw = "KRW"
+    case usd = "USD"
+}
---
+++ OpenMarket/OpenMarket/Model/JSONModel/ProductDetail.swift
@@ -0,0 +1,34 @@
+//
+//  ProductDetailList.swift
+//  OpenMarket
+//
+//  Created by bonf, kiwi on 2022/08/09.
+//
+
+struct ProductDetail: Decodable {
+    let id: Int
+    let vendorID: Int
+    let name: String
+    let thumbnail: String
+    let currency: Currency
+    let price: Int
+    let bargainPrice: Int
+    let discountedPrice: Int
+    let stock: Int
+    let createdAt: String
+    let issuedAt: String
+    
+    private enum CodingKeys: String, CodingKey {
+        case id
+        case vendorID = "vendor_id"
+        case name
+        case thumbnail
+        case currency
+        case price
+        case bargainPrice = "bargain_price"
+        case discountedPrice = "discounted_price"
+        case stock
+        case createdAt = "created_at"
+        case issuedAt = "issued_at"
+    }
+}
