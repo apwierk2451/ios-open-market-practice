9be1b28be563a1a291a81f1a1e1d250cf6dfd44f
apwierk2451 <apwierk2451@gmail.com>
2022-08-12 15:17:22 +0900

    test: Patch 기능 테스트 구현

--- OpenMarket/APIRequestTest/APIRequestTest.swift
+++ OpenMarket/APIRequestTest/APIRequestTest.swift
@@ -32,7 +32,7 @@ class APIRequestTest: XCTestCase {
     let boundary = "Boundary-\(UUID().uuidString)"
     
     override func setUpWithError() throws {
-      
+        sut = GetData(method: .get, baseURL: URLHost.openMarket.url, path: "")
     }
     
     override func tearDownWithError() throws {
@@ -82,6 +82,37 @@ class APIRequestTest: XCTestCase {
         let multiPartFormData = MultiPartForm(boundary: boundary, jsonData: productData, images: [Image(name: "귀요미", data: pngData, type: "png")])
         let networkManager = NetworkManager()
         let request = TestRequest(body: .multiPartForm(multiPartFormData) ,path: URLAdditionalPath.product.value, method: .post, baseURL: URLHost.openMarket.url, headers: ["identifier": UserInfo.identifier.text, "Content-Type": "multipart/form-data; boundary=\(boundary)"])
+        networkManager.dataTask(with: request) { result in
+            switch result {
+            case .success(let success):
+                print(String(decoding: success, as: UTF8.self))
+            case .failure(let error):
+                print(error.localizedDescription)
+                break
+            }
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 300)
+    }
+    
+    func test_PATCH_메서드_동작확인() {
+        //given
+        let expectation = expectation(description: "비동기테스트")
+       
+        let product = RegistrationProduct(name: "수정수정",
+                                          descriptions: "화이팅",
+                                          price: 15000.0,
+                                          currency: "USD",
+                                          discountedPrice: 0.0,
+                                          stock: 3,
+                                          secret: UserInfo.secret.text)
+        guard let productData = try? JSONEncoder().encode(product) else { return }
+        let networkManager = NetworkManager()
+        let request = TestRequest(body: .json(productData),
+                                  path: URLAdditionalPath.product.value + "/4523/",
+                                  method: .patch,
+                                  baseURL: URLHost.openMarket.url,
+                                  headers: ["identifier": UserInfo.identifier.text, "Content-Type": "application/json"])
         networkManager.dataTask(with: request) { result in
             switch result {
             case .success(let success):
